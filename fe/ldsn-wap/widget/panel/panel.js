e6c07de1c266f831790413969d744481d4910089
edire <az8641683@163.com>
2015-03-28 15:17:11 +0800

    添加通用浮层

--- fe/ldsn-wap/widget/panel/panel.js
+++ fe/ldsn-wap/widget/panel/panel.js
@@ -4,19 +4,30 @@
  * @date 2015-04-17
  */
 
-var _pub = {
+var _pri = {
     node: {
         panelMod: $('section[node-type="module-panel"]')
     },
+    bindListener: function () {
+        ldev.message.listen('clear_frame', function (){
+            _pub.close();
+        });
+    }
+}
+var init = function () {
+    _pri.bindListener();
+}
+init();
+var _pub = {
     build: function (tmpl, data) {
         var tplData = data || {};
         var html = ldev.tmpl(tmpl, data);
-        _pub.node.panelMod.html(html);
-        _pub.node.panelMod.addClass('active');
+        _pri.node.panelMod.html(html);
+        _pri.node.panelMod.addClass('active');
     },
     close: function () {
-        _pub.node.panelMod.removeClass('active');
-        _pub.node.panelMod.html();
-    }
+        _pri.node.panelMod.removeClass('active');
+        _pri.node.panelMod.empty();
+    },
 };
 module.exports = _pub;
