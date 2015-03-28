2f4308d4746dc15884d3d77c40e155ebb9f58dd4
edire <az8641683@163.com>
2015-03-28 15:13:28 +0800

    添加message

--- fe/ldsn-wap/static/message.js
+++ fe/ldsn-wap/static/message.js
@@ -7,7 +7,19 @@
 
 //以一下方式创建事件
 
-ldev.message.create('check-login');
+ldev.message.create('check_login');
 ldev.message.create('logout');
 ldev.message.create('login_end');
 ldev.message.create('logout_end');
+ldev.message.create('clear_frame');
+ldev.message.create('refresh_list');
+ldev.message.create('to_article');
+
+//下面是公用message
+
+ldev.message.listen('login_end', function () {
+    ldsn.loginStatus = true;
+});
+ldev.message.listen('logout_end', function () {
+    ldsn.loginStatus = false;
+});
